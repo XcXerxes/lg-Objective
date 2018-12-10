374f8795fff1643c56493bf6e5e91c6b38111d92
Smile <xcxerxes@aliyun.com>
2018-12-10 23:01:39 +0800

    Initial Commit

---
+++ lg-Objective/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lg-Objective
+//
+//  Created by xiacan on 2018/12/10.
+//  Copyright © 2018 iotek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lg-Objective/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lg-Objective
+//
+//  Created by xiacan on 2018/12/10.
+//  Copyright © 2018 iotek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
